42f2886751eb951bbcbd4f9f8902589ef17c2034
Mavioux <pakmarios@gmail.com>
2020-11-24 04:16:04 +0200

    deleted some comments

--- triangle_v3.c
+++ triangle_v3.c
@@ -1,27 +1,3 @@
-/* 
-*   Matrix Market I/O example program
-*
-*   Read a real (non-complex) sparse matrix from a Matrix Market (v. 2.0) file.
-*   and copies it to stdout.  This porgram does nothing useful, but
-*   illustrates common usage of the Matrix Matrix I/O routines.
-*   (See http://math.nist.gov/MatrixMarket for details.)
-*
-*   Usage:  a.out [filename] > output
-*
-*       
-*   NOTES:
-*
-*   1) Matrix Market files are always 1-based, i.e. the index of the first
-*      element of a matrix is (1,1), not (0,0) as in C.  ADJUST THESE
-*      OFFSETS ACCORDINGLY offsets accordingly when reading and writing 
-*      to files.
-*
-*   2) ANSI C requires one to use the "l" format modifier when reading
-*      double precision floating point numbers in scanf() and
-*      its variants.  For example, use "%lf", "%lg", or "%le"
-*      when reading doubles, otherwise errors will occur.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
